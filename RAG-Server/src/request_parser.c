0d636aa4fdcb679a1ecd824790dd5dbbad5157b2
lilalea <leaviktoriaeder@gmail.com>
2026-05-09 12:51:56 +0200

    added some more comments to what i just did

--- RAG-Server/src/request_parser.c
+++ RAG-Server/src/request_parser.c
@@ -1,27 +1,28 @@
+//including own .h file
 #include "../include/request_parser.h"
 
 #include <stdio.h>
 #include <string.h>
 
-//parses raw http request string and extracts
-//method, path and http version
-//returns 1 if request is valid, if not valid returns 0
+// parses raw http request and stores method, path and http version in struct
 int parse_http_request(const char* raw_request, HttpRequest* request) {
     if (sscanf(raw_request, "%7s %255s %15s",
         request->method,
         request->path,
         request->version) != 3) {
+
+        // invalid request
         return 0;
     }
-
+    //only supports http/1.1 requests
     if (strcmp(request->version, "HTTP/1.1") != 0) {
         return 0;
     }
-
+    //allows supported http methods
     if (strcmp(request->method, "GET") != 0 &&
         strcmp(request->method, "POST") != 0) {
         return 0;
     }
-
+    // parsed successfully
     return 1;
 }
