ead3bd6c5e0688e47063089b2db4a8586079ddec
lxnx1402 <lenabeckmann1@gmail.com>
2026-06-10 15:43:55 +0200

    added chromadb to docker

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "Setting up RAG System..."
 
 # Create data directories
-mkdir -p data/llm data/documents
+mkdir -p data/llm data/documents data/chromadb
 
 # Set permissions
 chmod -R 755 data/
@@ -12,14 +12,15 @@ chmod -R 755 data/
 docker compose down --remove-orphans
 
 # Port freigeben falls nötig
-#sudo pkill -f rag-llm
+sudo pkill -f rag-llm
+sudo pkill -f rag-chromadb
 sudo pkill -f rag-server
 docker rm -f rag-server
 
 # Build and start services
 docker compose up --build -d
 
-#docker exec -it rag-llm ollama pull qwen3:0.6b
+docker exec -it rag-llm ollama pull qwen3:0.6b
 
 docker build -t rag-server RAG-Server/.
 docker run --name rag-server rag-server
